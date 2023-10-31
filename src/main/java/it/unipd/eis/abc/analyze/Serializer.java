43bbf60f056648427d6e062b8ec2d3d8284dc1bd
giovannifaedo <giovannipuntofaedo73@gmail.com>
2023-10-31 16:30:43 +0100

    Add Deserializer and Serializer class in analyze folder.

---
+++ src/main/java/it/unipd/eis/abc/analyze/Serializer.java
@@ -0,0 +1,9 @@
+package it.unipd.eis.abc.analyze;
+
+/**
+ * Classe per serializzare gli articoli, ovvero mappare un insieme di oggetti di tipo
+ * Article in un file JSON
+ */
+public class Serializer {
+
+}
