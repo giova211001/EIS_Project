044f8523a50ebb4d700a010dfa453dacbf96c7bd
giovannifaedo <giovannipuntofaedo73@gmail.com>
2023-11-02 21:11:13 +0100

    Add first implement of App.java (to manage the interaction of the user in the CLI).

--- src/main/java/it/unipd/eis/abc/App.java
+++ src/main/java/it/unipd/eis/abc/App.java
@@ -1,5 +1,9 @@
 package it.unipd.eis.abc;
 
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+
 /**
  * Classe per eseguire il programma e gestire l'interazione dell'utente con la CLI (Command Line Interface)
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
+        Options opt = new Options();
+        //importo tutte le possibili azioni in una actionGroup
+        OptionGroup action = new OptionGroup();
+        action.addOption(new Option("h", "help", false, "Print the help"));
+        action.addOption(new Option("e", "extraction", true, "Extract terms"));
+
+        action.setRequired(true);
+        opt.addOptionGroup(action);
+
+        //possibili opzioni
+        opt.addOption(new Option("pf", true, "Property file path"));
+        opt.addOption(new Option("np", "nlp-pipeline", true, "CoreNLP"));
+
+
 
     }
 }
